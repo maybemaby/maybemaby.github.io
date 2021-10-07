0fef68c0af0de805f058b701cbb2cfeb9515d9be
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-10-06 18:49:51 -0700

    Switcher changes section displayed.

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,22 @@ const setActiveSection = (event) => {
     section.className = section.className.replace(/\s?active/, "");
   }
   event.currentTarget.className += " active"
+  changeVisibleSection(event.currentTarget.id);
+}
+
+const changeVisibleSection = (id) => {
+  for (const section of document.querySelectorAll('section#showcase section')) {
+    // If the section clicked on already is displaying, return early
+    if (section.id === id && getComputedStyle(section).display === 'block') {
+      return
+    }
+    else {
+      section.style.display = 'none';
+      if (section.id === id) {
+        section.style.display = 'block';
+      }
+    }
+  }
 }
 
 for (const section of sections) {
