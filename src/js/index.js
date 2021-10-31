15cb391f601a2a56271734818b54d2ee8da9eeca
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-10-31 03:41:34 -0700

    Add animation to tab indicator

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
 const sections = document.querySelectorAll(".switch-button button");
 const projectSection = document.querySelector("section#projects");
+const indicator = document.querySelector('div#nav-indicator');
 
 const setActiveSection = (event) => {
-  console.log(event.currentTarget);
   for (const section of sections) {
     section.className = section.className.replace(/\s?active/, "");
   }
   event.currentTarget.className += " active";
   changeVisibleSection(event.currentTarget.id);
+  switch (event.currentTarget.id) {
+    case 'experience':
+      indicator.style.left = "0%"
+      break;
+    case 'projects':
+      indicator.style.left = "32%"
+      break;
+    case 'skills':
+      indicator.style.left = "64%"
+      break;
+    default:
+      return;
+  }
 };
 
 const changeVisibleSection = (id) => {
@@ -35,12 +48,22 @@ const projects = [
     summary: `Personal dashboard with charts and tables made for a family member to track their investments from their financial advisor.\
     Uses a Django backend with PostgreSQL and Celery for task queue. React frontend with Material UI. Deployed with Docker on a Linux VPS.`,
     preview: "",
+    repo: 'https://github.com/maybemaby/maybemaby.github.io'
   },
   {
     title: "CantDecide",
-    summary: `React app for helping organize decision making and dealing with decision paralysis.`,
+    summary: `React app for helping organize decision making and dealing with decision paralysis.\
+    Break choices down into factors weighted by the user and calculates each choice's score.\
+    Styled with Tailwind, testing pipeline with Jest and Github Actions.`,
     preview: "",
+    repo: ''
   },
+  {
+    title: "Personal Portfolio (this website)",
+    summary: 'Showcase for my projects. Made with vanilla HTML, CSS, and Javascript',
+    preview: '',
+    repo: ''
+  }
 ];
 
 // Populates the project section with cards highlighting projects
